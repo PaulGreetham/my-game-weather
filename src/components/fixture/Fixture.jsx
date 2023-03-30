1bdcd25ce85e3a0f2918b667efe94bdcb712e347
Paul Greetham <pgreetham@protonmail.com>
2023-03-30 15:01:18 +0200

    fixture card detials updated

--- src/components/fixture/Fixture.jsx
+++ src/components/fixture/Fixture.jsx
@@ -3,8 +3,12 @@ import './Fixture.scss';
 
 function Fixture(props) {
   const { fixture, onFixtureSelect } = props;
-  const homeTeam = fixture.teams.home || {}; // changed from fixture.homeTeam to fixture.teams.home
-  const awayTeam = fixture.teams.away || {}; // changed from fixture.awayTeam to fixture.teams.away
+  const homeTeam = fixture.teams.home || {};
+  const awayTeam = fixture.teams.away || {};
+
+  const fixtureDate = new Date(fixture.fixture.date);
+  const fixtureDateString = fixtureDate.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  const fixtureTimeString = fixtureDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
 
   return (
     <div className="fixture-card" onClick={() => onFixtureSelect(fixture)}>
@@ -13,14 +17,15 @@ function Fixture(props) {
         <img src={awayTeam.logo} alt={awayTeam.name} />
       </div>
       <div className="team-names">
-        <span>{homeTeam.name || 'N/A'}</span>
-        <span>{awayTeam.name || 'N/A'}</span>
+        <div className="team-name">{homeTeam.name || 'N/A'}</div>
+        <div className="vs">v</div>
+        <div className="team-name">{awayTeam.name || 'N/A'}</div>
       </div>
       <div className="fixture-details">
-        <div className="fixture-date">{fixture.fixture.date}</div> {/* Added fixture prefix */}
-        <div className="fixture-status">{fixture.fixture.status?.short || 'N/A'}</div> {/* Added fixture prefix */}
-        <div className="fixture-venue">{fixture.fixture.venue?.name || 'N/A'}</div> {/* Added fixture prefix */}
-        <div className="fixture-competition">{fixture.league?.name || 'N/A'}</div> {/* Replaced fixture.competition with league */}
+        <div className="fixture-date">{fixtureDateString}</div>
+        <div className="fixture-time">{fixtureTimeString}</div>
+        <div className="fixture-venue">{fixture.fixture.venue?.name || 'N/A'}</div>
+        <div className="fixture-competition">{fixture.league?.name || 'N/A'}</div>
       </div>
     </div>
   );
